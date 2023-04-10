0c9052d92994895875e1685a5858e595736d2cb2
Shell-by <wnsghks1026@naver.com>
2023-04-10 17:57:25 +0900

    feat: Add chart.js and todo chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { TuiCalendar } from "./Pages/index";
+import TodoChart from "./Components/TodoChart";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
             <Route path="/calendar" element={<TuiCalendar />} />
+            <Route path="/todoChart" element={<TodoChart />} />
         </Routes>
       </BrowserRouter>
     </div>
