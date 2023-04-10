2b3a6d46401877dc7f79411b68f94a8bedb94706
MINGYU <90837142+mingyu-0713@users.noreply.github.com>
2023-04-10 20:21:39 +0900

    0410

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import "../Styles/login.css"
+import React from "react";
+import "../Styles/login.css";
+import logo from "../images/로고.png";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
 
 const Login = () => {
   return (
-    <div className='login-container'>
-        <div className='login-form'>
-            
+    <div className="login-container">
+      <div className="login-form">
+        <div className="logo-div">
+          <img src={logo} alt="logo" className="paru-chat-img" />
+          <p className="paru-chat-text">Paru Chat</p>
         </div>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Email address"
+          className="mb-3"
+        >
+          <Form.Control type="email" placeholder="name@example.com" />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingPassword" label="Password">
+          <Form.Control type="password" placeholder="Password" />
+        </FloatingLabel>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
