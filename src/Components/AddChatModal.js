2cf01db34f2d0738e63d0eedf9e85448d7f9ef3f
MINGYU <90837142+mingyu-0713@users.noreply.github.com>
2023-04-24 20:27:09 +0900

    0424

--- src/Components/AddChatModal.js
+++ src/Components/AddChatModal.js
@@ -1,34 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
-import "../Styles/modal_addChat_first.css"
+import "../Styles/modal_addChat_first.css";
 
 const AddChatModal = () => {
+  const fileTypes = ["JPEG", "PNG", "JPG"];
 
   const [modalOpen, setModalOpen] = useState(false);
 
-    const ChatToClose = () => {
-        setModalOpen(false);
-    };
-
-
+  const ChatToClose = () => {
+    setModalOpen(false);
+  };
 
   return (
-    <div className='AddChatModal-container' onClick={ChatToClose}>
-      <div className='modal-body' onClick={(e) => e.stopPropagation()}>
-        <p className='addChat-text'>채팅방 만들기</p>
-        <button type='submit'>
-        <AiOutlineClose className='closeBtn' onClick={ChatToClose}/>
+    <div className="AddChatModal-container" onClick={ChatToClose}>
+      <div className="modal-body" onClick={(e) => e.stopPropagation()}>
+        <p className="addChat-text">채팅방 만들기</p>
+        <button type="submit">
+          <AiOutlineClose className="closeBtn" onClick={ChatToClose} />
         </button>
-        <div className='chat-item-div'>
-          <span>채팅방 이름</span>
-          <input type='text' placeholder='채팅방 이름을 입력하세요' name='chat_room_name'/>
+        <div className="chat-item-div">
+          <span className="chat-room_name-text">채팅방 이름</span>
+          <input
+            type="text"
+            placeholder="채팅방 이름을 입력하세요"
+            name="chat_room_name"
+            className="chat-room_name"
+          />
 
-          <span>채팅방 사진</span>
-          
+          <span className="chat-room_img-text">채팅방 사진</span>
+          <div className="file-layout-div">
+          </div>
+
+          <button type="submit" className="chat-room-next-btn">
+            다음
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AddChatModal;
