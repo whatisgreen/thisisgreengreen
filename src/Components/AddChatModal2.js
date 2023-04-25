765c44ba1669b9f49525ba69c0544710111d7c10
MINGYU <90837142+mingyu-0713@users.noreply.github.com>
2023-04-25 20:27:19 +0900

    0425

--- src/Components/AddChatModal2.js
+++ src/Components/AddChatModal2.js
@@ -1,15 +1,35 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { AiOutlineClose } from "react-icons/ai";
 import "../Styles/modal_addChat_second.css";
 
-const AddChatModal2 = () => {
+const AddChatModal2 = ({ name, email }) => {
 
-    const [user, setUser] = useState({
-        name: "",
-        email: "",
-    }, []);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const ChatToClose = () => {
+    setModalOpen(false);
+  }
 
   return (
-    <div>AddChatModal2</div>
+    <div className='AddChatModal2' onClick={ChatToClose}>
+      <div className='modal-body2' onClick={(e) => e.stopPropagation()}>
+        <p className='addChat-text2'>채팅방 만들기</p>
+        <button type='submit'>
+          <AiOutlineClose className='closeBtn2' onClick={ChatToClose} />
+        </button>
+        <div className='chat-item-div2'>
+          <span className='chat-room_user-invite-text'>사용자 초대</span>
+          <input
+            type="text"
+            placeholder='박민규#0713'
+            name='chat-room_user-invite'
+            className='chat-room_user-invite'  
+          />
+
+          <div></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
