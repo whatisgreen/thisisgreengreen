821c1ca3208be4417cdaf10470c6a53d122c9c4a
MINGYU <90837142+mingyu-0713@users.noreply.github.com>
2023-04-25 19:53:39 +0900

    0425

---
+++ src/Components/AddChatModal2.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import "../Styles/modal_addChat_second.css";
+
+const AddChatModal2 = () => {
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+    }, []);
+
+  return (
+    <div>AddChatModal2</div>
+  )
+}
+
+export default AddChatModal2;
