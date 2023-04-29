15850bccf836c49cb5f66f8bd07823ad42c6bbff
MINGYU <90837142+mingyu-0713@users.noreply.github.com>
2023-04-29 12:04:33 +0900

    0429

--- src/Components/AddChatModal2.js
+++ src/Components/AddChatModal2.js
@@ -1,42 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { IoMdAddCircleOutline } from "react-icons/io";
 import "../Styles/modal_addChat_second.css";
 
-const AddChatModal2 = ({ name, email }) => {
+const AddChatModal2 = (props) => {
+  const { user } = props;
 
   const [modalOpen, setModalOpen] = useState(false);
 
   const ChatToClose = () => {
     setModalOpen(false);
-  }
+  };
 
   return (
-    <div className='AddChatModal2' onClick={ChatToClose}>
-      <div className='modal-body2' onClick={(e) => e.stopPropagation()}>
-        <p className='addChat-text2'>채팅방 만들기</p>
-        <button type='submit'>
-          <AiOutlineClose className='closeBtn2' onClick={ChatToClose} />
+    <div className="AddChatModal2" onClick={ChatToClose}>
+      <div className="modal-body2" onClick={(e) => e.stopPropagation()}>
+        <p className="addChat-text2">채팅방 만들기</p>
+        <button type="submit">
+          <AiOutlineClose className="closeBtn2" onClick={ChatToClose} />
         </button>
-        <div className='chat-item-div2'>
-          <span className='chat-room_user-invite-text'>사용자 초대</span>
-          <span><IoMdAddCircleOutline className='IoMdAddCircleOutline'/></span>
+        <div className="chat-item-div2">
+          <span className="chat-room_user-invite-text">사용자 초대</span>
+          <span>
+            <IoMdAddCircleOutline className="IoMdAddCircleOutline" />
+          </span>
           <input
             type="text"
-            placeholder='박민규#0713'
-            name='chat-room_user-invite'
-            className='chat-room_user-invite'  
+            placeholder="박민규#0713"
+            name="chat-room_user-invite"
+            className="chat-room_user-invite"
           />
 
-          <div className="chat-room_user-div"></div>
-        
+          <div className="chat-room_user-div">
+            <span></span> <span></span>
+          </div>
+
           <button type="submit" className="chat-room-finish-btn">
             만들기
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AddChatModal2;
